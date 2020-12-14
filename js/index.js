fdde839601ecfa1529c24a147cf6a29af732ddc1
doej96 <doej96@naver.com>
2020-12-14 13:43:23 +0900

    ..............................

--- js/index.js
+++ js/index.js
@@ -16,16 +16,49 @@ $.get('../json/new-products.json', onNewProducts); // new releases 상품 가져
 /******** 이벤트 콜백 *********/
 function onNewProducts(r) {
     console.log(r);
+    for (var i = 0, html = ''; i < r.length; i++) {
+        html += '<div class="slide">'
+        html += '<div class="img-wrap">'
+        html += '<img src="'+ r[i].src +'" alt="상품" class="w-100">'
+        html += '</div>'
+        html += '<div class="content-wrap">'
+        html += '<h4 class="title">' + r[i].title + '</h4>'
+        html += '<p class="summary">' + r[i].summary + '</p>'
+        html += '<div class="star">'
+        for (var j = 1; j <= 5; j++) {
+            if (r[i].star == 0) {
+                html += '<i class="fa fa-star"></i>'
+            } else if (r[i].star >= j) {
+                if (r[i].star >= j + 0.3 && r[i].star <= j + 0.7) {
+                    html += '<i class="fa fa-star-half active"></i>'
+                } else
+                    html += '<i class="fa fa-star active"></i>'
+            }
+        }
+        html += '</div>'
+        html += '<div class="content">'
+        html += '<span class="price-original">$' + r[i].originalPrice + '</span>'
+        html += '<span>|</span>'
+        html += '<span class="origin">' + r[i].origin + '</span>'
+        html += '</div>'
+        html += '<div class="price-sale">$' + r[i].salePrice + '</div>'
+        html += '</div>'
+        html += '</div>'
+        $(".navi-new .slide-container").append(html);
+    }
 }
+
 function onLangChg() {
     $(".trans-wrapper").stop().slideToggle(200);
     $(".trans-wrapper .lang-sel").stop().slideUp(200); /* .trans-wrapper 닫힐 때 .lang-sel 열려 있어도 닫힘 */
 }
+
 function onLangSel() {
-    if($(this).next().css("display") === 'none'){  /* css속성이 display인 값 찾아서(getter) none이면 */
+    if ($(this).next().css("display") === 'none') {
+        /* css속성이 display인 값 찾아서(getter) none이면 */
         $(".trans-wrapper .lang-sel").stop().slideUp(200);
         $(this).next().slideDown(200);
-    }else{
+    } else {
         $(".trans-wrapper .lang-sel").stop().slideUp(200); /* 열려있는 상태에서 한 번 더 눌렀을 때 */
     }
 }
