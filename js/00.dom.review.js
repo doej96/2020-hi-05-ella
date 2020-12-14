891c4892cdad42d6f237e09d8e4efbc0b75701dd
doej96 <doej96@naver.com>
2020-12-14 17:01:36 +0900

    ...

--- js/00.dom.review.js
+++ js/00.dom.review.js
@@ -1,9 +1,39 @@
 
+
+var colors = ['green','red','blue','pink','orange'];
+
 function onCreateBox() {
-var cnt = Number($("input[name='cnt']").val());
-for(var i=0; i<cnt; i++) {
-	$(".wrapper").append('<div class="box"></div>');
+	var cnt = Number($("input[name='cnt']").val());
+	for(var i=0, color; i<cnt; i++){
+		var color = Math.ceil(Math.random() * 5)-1;
+		$(".wrapper").append('<div class="box '+colors[color]+'"></div>')
+	}
 }
+
+function onResetBox() {
+	$(".wrapper").empty();
 }
 
 $("#btCreate").click(onCreateBox);
+$("#btReset").click(onResetBox);
+
+
+/* 
+var colors = ['pink', 'green', 'blue', 'orange', 'red'];
+
+function onCreateBox(){
+var cnt = Number($("input[name='cnt']").val());
+for(var i=0, color; i<cnt; i++) {
+	var color = Math.ceil(Math.random() * 5)-1
+	console.log(color);
+	$(".wrapper").append('<div class="box '+colors[color]+'"></div>')
+}
+}
+
+function onResetBox(){
+	$(".wrapper").empty();
+}
+
+$("#btCreate").click(onCreateBox);
+$("#btReset").click(onResetBox);
+ */
