336f29d496a9ae1650625497f56e368e30d75c1d
doej96 <doej96@naver.com>
2020-12-14 12:31:35 +0900

    ㅜㅠ...진짜 가만안둔다

--- js/index.js
+++ js/index.js
@@ -1,76 +1,4 @@
 /******** 전역선언 *********/
-var products = [
-    {
-    id:1,
-    link: "#",
-    title: "MARK",
-    summary: "Dinterdum Pretium De Condimentus",
-    star: 5.0,
-    originalPrice: 199.00,
-    origin: "from",
-    src: "../img/pic1.jpg"
-},
-{
-    id:2,
-    link: "#",
-    title: "G-STAR",
-    summary: "Dinterdum Pretium De Condimentus",
-    star: 4.5,
-    originalPrice: 189.00,
-    salePrice: "",
-    origin: "",
-    src: "../img/pic2.jpg"
-},
-{
-    id:3,
-    link: "#",
-    title: "CHANEL",
-    summary: "Dinterdum Pretium De Condimentus",
-    star: 5.0,
-    originalPrice: 215.00,
-    salePrice: 192.10,
-    origin: "",
-    src: "../img/pic3.jpg"
-},
-{
-    id:4,
-    link: "#",
-    title: "BURBERRY",
-    summary: "Dinterdum Pretium De Condimentus",
-    star: 4,
-    originalPrice: 189.00,
-    salePrice: 0,
-    origin: "",
-    src: "../img/pic4.jpg"
-},
-{
-    id:5,
-    link: "#",
-    title: "BURBERRY",
-    summary: "Dinterdum Pretium De Condimentus",
-    star: 3,
-    originalPrice: 90.00,
-    salePrice: 0,
-    origin: "",
-    src: "../img/pic5.jpg"
-},
-{
-    id:6,
-    link: "#",
-    title: "BENJAMIN BUTTON",
-    summary: "Dinterdum Pretium De Condimentus",
-    star: 5,
-    originalPrice: 189.00,
-    salePrice: 0,
-    origin: "",
-    src: "../img/pic6.jpg"
-}
-];
-
-var json = JSON.stringify(products); //객체에 ''넣어서 문자열로 만듦
-var js = JSON.parse(json);
-console.log(json);
-console.log(js);
 
 
 
@@ -82,9 +10,13 @@ console.log(js);
 /******** 이벤트 선언 *********/
 $('.top-wrapper .icon-down').click(onLangChg); // 언어선택
 $('.top-wrapper .bt-down').click(onLangSel); // 언어선택
+$.get('../json/new-products.json', onNewProducts); // new releases 상품 가져오기
 
 
 /******** 이벤트 콜백 *********/
+function onNewProducts(r) {
+    console.log(r);
+}
 function onLangChg() {
     $(".trans-wrapper").stop().slideToggle(200);
     $(".trans-wrapper .lang-sel").stop().slideUp(200); /* .trans-wrapper 닫힐 때 .lang-sel 열려 있어도 닫힘 */
