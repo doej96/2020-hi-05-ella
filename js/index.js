babb65eb00c7307f45502bcf9986531e4661de4d
doej96 <doej96@naver.com>
2020-12-24 15:41:03 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -6,6 +6,15 @@ function renderPrd() {
 	$(".prd").each(function(i){
 		var discount = $(this).data('discount');
 		var icon = $(this).data('icon');
+		var html='';
+		if(discount) {
+			html+= '<div class="discount>'+discount+'</div>';
+		}
+		if(icon && icon.length > 0) { /* 앞의 조건을 먼저 검사 */
+			for(var i in icon) {
+				html += '<div class="icon" style="background-color: '+icon[i].bg+';">'+icon[i].title+'</div>'
+			}
+		}
 	})
 }
 
